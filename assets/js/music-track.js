ff8135ae4481572cb08a9104065b3b56ff9afc49
SrGobi <62013924+SrGobi@users.noreply.github.com>
2021-11-27 04:24:39 +0100

    Add Music PopUp Traking

--- assets/js/music-track.js
+++ assets/js/music-track.js
@@ -2,7 +2,6 @@ $(function () {
   var playerTrack = $("#player-track"),
     bgArtwork = $("#bg-artwork"),
     bgArtworkUrl,
-    albumName = $("#album-name"),
     trackName = $("#track-name"),
     albumArt = $("#album-art"),
     sArea = $("#s-area"),
@@ -29,30 +28,26 @@ $(function () {
     nTime = 0,
     buffInterval = null,
     tFlag = false,
-    albums = [
-      "Dawn",
-      "Me & You",
-      "Electro Boy",
-      "Home",
-      "Proxy (Original Mix)"
-    ],
     trackNames = [
-      "Skylike - Dawn",
-      "Alex Skrindo - Me & You",
+      "Flaix FM",
+      "Los 40 FM",
       "Kaaze - Electro Boy",
-      "Jordan Schor - Home",
+      "Kiss FM",
       "Martin Garrix - Proxy"
     ],
     albumArtworks = ["_1", "_2", "_3", "_4", "_5"],
     trackUrl = [
-      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/2.mp3",
-      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/1.mp3",
+      "http://nodo03-cloud01.streaming-pro.com:8000/flaixfm.mp3",
+      "https://20723.live.streamtheworld.com/LOS40_URBANAAC.aac",
       "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/3.mp3",
-      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/4.mp3",
+      "https://kissfm.kissfmradio.cires21.com/kissfm.mp3",
       "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/5.mp3"
     ],
     playPreviousTrackButton = $("#play-previous"),
     playNextTrackButton = $("#play-next"),
+    volumeRange = $("#volume-range"),
+    volumeDown = $("#volume-down"),
+    volumeUp = $("#volume-up"),
     currIndex = -1;
 
   function playPause() {
@@ -78,24 +73,16 @@ $(function () {
     seekBarPos = sArea.offset();
     seekT = event.clientX - seekBarPos.left;
     seekLoc = audio.duration * (seekT / sArea.outerWidth());
-
     sHover.width(seekT);
-
     cM = seekLoc / 60;
-
     ctMinutes = Math.floor(cM);
     ctSeconds = Math.floor(seekLoc - ctMinutes * 60);
-
     if (ctMinutes < 0 || ctSeconds < 0) return;
-
     if (ctMinutes < 0 || ctSeconds < 0) return;
-
     if (ctMinutes < 10) ctMinutes = "0" + ctMinutes;
     if (ctSeconds < 10) ctSeconds = "0" + ctSeconds;
-
     if (isNaN(ctMinutes) || isNaN(ctSeconds)) insTime.text("--:--");
     else insTime.text(ctMinutes + ":" + ctSeconds);
-
     insTime.css({ left: seekT, "margin-left": "-21px" }).fadeIn(0);
   }
 
@@ -113,32 +100,23 @@ $(function () {
   function updateCurrTime() {
     nTime = new Date();
     nTime = nTime.getTime();
-
     if (!tFlag) {
       tFlag = true;
       trackTime.addClass("active");
     }
-
     curMinutes = Math.floor(audio.currentTime / 60);
     curSeconds = Math.floor(audio.currentTime - curMinutes * 60);
-
     durMinutes = Math.floor(audio.duration / 60);
     durSeconds = Math.floor(audio.duration - durMinutes * 60);
-
     playProgress = (audio.currentTime / audio.duration) * 100;
-
     if (curMinutes < 10) curMinutes = "0" + curMinutes;
     if (curSeconds < 10) curSeconds = "0" + curSeconds;
-
     if (durMinutes < 10) durMinutes = "0" + durMinutes;
     if (durSeconds < 10) durSeconds = "0" + durSeconds;
-
     if (isNaN(curMinutes) || isNaN(curSeconds)) tProgress.text("00:00");
     else tProgress.text(curMinutes + ":" + curSeconds);
-
     if (isNaN(durMinutes) || isNaN(durSeconds)) tTime.text("00:00");
     else tTime.text(durMinutes + ":" + durSeconds);
-
     if (
       isNaN(curMinutes) ||
       isNaN(curSeconds) ||
@@ -147,9 +125,7 @@ $(function () {
     )
       trackTime.removeClass("active");
     else trackTime.addClass("active");
-
     seekBar.width(playProgress + "%");
-
     if (playProgress == 100) {
       i.attr("class", "fa fa-play");
       seekBar.width(0);
@@ -173,29 +149,22 @@ $(function () {
   function selectTrack(flag) {
     if (flag == 0 || flag == 1) ++currIndex;
     else --currIndex;
-
     if (currIndex > -1 && currIndex < albumArtworks.length) {
       if (flag == 0) i.attr("class", "fa fa-play");
       else {
         albumArt.removeClass("buffering");
         i.attr("class", "fa fa-pause");
       }
-
       seekBar.width(0);
       trackTime.removeClass("active");
       tProgress.text("00:00");
       tTime.text("00:00");
-
-      currAlbum = albums[currIndex];
       currTrackName = trackNames[currIndex];
       currArtwork = albumArtworks[currIndex];
-
       audio.src = trackUrl[currIndex];
-
       nTime = 0;
       bTime = new Date();
       bTime = bTime.getTime();
-
       if (flag != 0) {
         audio.play();
         playerTrack.addClass("active");
@@ -204,8 +173,6 @@ $(function () {
         clearInterval(buffInterval);
         checkBuffering();
       }
-
-      albumName.text(currAlbum);
       trackName.text(currTrackName);
       albumArt.find("img.active").removeClass("active");
       $("#" + currArtwork).addClass("active");
@@ -218,26 +185,29 @@ $(function () {
       else ++currIndex;
     }
   }
+    function handleVolumeDown() {
+    volumeRange.value = Number(volumeRange.value) - 20
+    music.volume = volumeRange.value / 100
+    }
+    function handleVolumeUp() {
+    volumeRange.value = Number(volumeRange.value) + 20
+    music.volume = volumeRange.value / 100
+    }
 
   function initPlayer() {
     audio = new Audio();
-
     selectTrack(0);
-
-    audio.loop = false;
-
+    audio.autoplay = true;
+    audio.loop = true;
     playPauseButton.on("click", playPause);
-
+    volumeDown.on('click', handleVolumeDown);
+    volumeUp.on('click', handleVolumeUp);
     sArea.mousemove(function (event) {
       showHover(event);
     });
-
     sArea.mouseout(hideHover);
-
     sArea.on("click", playFromClickedPos);
-
     $(audio).on("timeupdate", updateCurrTime);
-
     playPreviousTrackButton.on("click", function () {
       selectTrack(-1);
     });
@@ -245,6 +215,5 @@ $(function () {
       selectTrack(1);
     });
   }
-
   initPlayer();
 });
